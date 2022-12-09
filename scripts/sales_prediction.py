14bc8eda5c30af5129bbe6d1c94326148bd139dc
Collin Brown <CJBrown7966@gmail.com>
2022-12-08 16:33:18 -0800

    Commented More on the Sales Prediction Script

--- scripts/sales_prediction.py
+++ scripts/sales_prediction.py
@@ -4,81 +4,90 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import OneHotEncoder, MaxAbsScaler
 from scipy.sparse import hstack
 
-# First, we need to read our data from its csv file.
-source = "data/vgsales.csv"
-print(f"Reading training data from {source}.")
-df = pd.read_csv(source)
+def main():
+    source = "data/vgsales.csv"                     # We're going to set the path to our data here, just for testing purposes.
+    print(f"Reading training data from {source}.")  # We'll print the path as well, just to provide some transparency.
+    data = pd.read_csv(source)                      # Here, we actually get around to reading the data from the source file.
 
-# Next, we select the variables we want to train on. For now, these are the titles, years, platforms, genres, and publishers
-# of each game.
-X = df[["Name", "Year", "Platform", "Genre", "Publisher"]].rename(columns={
-    "Name": "name",
-    "Year": "year",
-    "Platform": "platform",
-    "Genre": "genre",
-    "Publisher": "publisher"
-})
+    if not (data):
+        print("Unable to read data source file. Please make sure you have the dataset in the correct position and try again.")
+        return
 
-print("Selecting the data we're going to train on from our dataset...")
+    # Next, we select the variables we want to train on. For now, these are the titles, years, platforms, genres, and publishers
+    # of each game.
+    X = data[["Name", "Year", "Platform", "Genre", "Publisher"]].rename(columns={
+        "Name": "name",
+        "Year": "year",
+        "Platform": "platform",
+        "Genre": "genre",
+        "Publisher": "publisher"
+    })                                              # We rename these only because I'm used to handling lower case and I don't want
+                                                    # to produce any errors later by having them be upper case.
 
-# Then we select the target variable. For now, we're focusing on global scales, but we can add individual countries' sales later.
-y = df["Global_Sales"]
+    print("Selecting the data we're going to train on from our dataset...")
 
-# Because we want the user to be able to input arbitrary game titles, we need to do some wizardry to the titles.
-game_titles = X["name"].values.tolist()
+    # Then we select the target variable. For now, we're focusing on global scales, but we can add individual countries' sales later.
+    y = data["Global_Sales"]
 
-# We have to vectorize the names so that they can be used as features in our model.
-print("Vectorizing game titles...")
-vectorizer = CountVectorizer()
-vectorizer.fit(game_titles)
-name_vectors = vectorizer.transform(game_titles)
+    # Because we want the user to be able to input arbitrary game titles, we need to do some wizardry to the titles.
+    gameTitles = X["name"].values.tolist()
 
-# Then we can hand the rest of our data to OneHotEncoder.
-# We have to separate our other data from our title data and transform it.
-print("Encoding year, platform, genre, and publisher data...")
-encoder = OneHotEncoder(handle_unknown="ignore")
-X_other = X.drop("name", axis=1)
-encoder.fit(X_other)
-other_vectors = encoder.transform(X_other)
+    # We have to vectorize the names so that they can be used as features in our model.
+    print("Vectorizing game titles...")
+    vectorizer = CountVectorizer()
+    vectorizer.fit(gameTitles)
+    nameVectors = vectorizer.transform(gameTitles)
 
-# Now we're going to concatenate both back together.
-print("Concatenating data...")
-X_encoded = hstack([name_vectors, other_vectors])
+    # Then we can hand the rest of our data to OneHotEncoder.
+    # We have to separate our other data from our title data and transform it.
+    print("Encoding year, platform, genre, and publisher data...")
+    encoder = OneHotEncoder(handle_unknown="ignore")
+    xOther = X.drop("name", axis=1)
+    encoder.fit(xOther)
+    otherVectors = encoder.transform(xOther)
 
-# And now we're going to normalize the data so that our predictions can be more accurate.
-print("Normalizing data...")
-scaler = MaxAbsScaler()
-scaler.fit(X_encoded)
-X_normalized = scaler.transform(X_encoded)
+    # Now we're going to concatenate both back together.
+    print("Concatenating data...")
+    xEncoded = hstack([nameVectors, otherVectors])
 
-# We went with a Random Forest model because our data is skewed and abnormal and a regular
-# multiple regression model wouldn't provide accurate predictions.
-print("Training random forest regression model on our data...")
-model = RandomForestRegressor(n_estimators=10)
-X_normalized = X_normalized[:len(y)]
-model.fit(X_normalized, y)
-print("Model trained!")
+    # And now we're going to normalize the data so that our predictions can be more accurate.
+    print("Normalizing data...")
+    scaler = MaxAbsScaler()
+    scaler.fit(xEncoded)
+    xNormalized = scaler.transform(xEncoded)
 
-# Now we need to ask the user to input the data for the game we're going to predict the sales of.
-title = input("Enter the title of your game: ")
-year = int(input("Enter the year the game was released: "))
-platform = input("Enter the platform the game was released on: ")
-genre = input("Enter the genre of your game: ")
-publisher = input("Enter the publisher of your game: ")
+    # We went with a Random Forest model because it provides more accurate predictions
+    # for the kind of data we're dealing with.
+    print("Training random forest regression model on our data...")
+    model = RandomForestRegressor(n_estimators=10)
+    xNormalized = xNormalized[:len(y)]
+    model.fit(xNormalized, y)
+    print("Model trained!")
 
-# Check if the input data contains any NaN values
-if any(pd.isnull([title, year, platform, genre, publisher])):
-    print("One or more of the input values is invalid. Please try again.")
-else:
+    # Now we need to ask the user to input the data for the game we're going to predict the sales of.
+    # We should ideally change this when we get around to creating a user interface (if we do).
+    title = input("Enter the title of your game: ")
+    year = int(input("Enter the year the game was released: "))
+    platform = input("Enter the platform the game was released on: ")
+    genre = input("Enter the genre of your game: ")
+    publisher = input("Enter the publisher of your game: ")
+
+    # Check if the input data contains any NaN values
+    if any(pd.isnull([title, year, platform, genre, publisher])):
+        print("One or more of the input values is invalid. Please try again.")
+        return
+
     print("Thank you!")
 
     # Again, we have to do some magic to the title to transform it into usable data.
     print("Processing user inputs...")
 
-    game_title_vector = vectorizer.transform([title])
+    # We were running into a pernicious error, where OneHotEncoder would complain that
+    # it was fitted to data with feature names while the data being predicted lacked
+    # names, so we've added this to fix that:
+    gameTitleVector = vectorizer.transform([title])
 
-    # We need to create a DataFrame that contains the other data for the game the user has input.
-    game_other = pd.DataFrame(data={
+    gameOther = pd.DataFrame(data={
         "year": [year],
         "platform": [platform],
         "genre": [genre],
@@ -86,21 +95,26 @@ else:
     })
 
     # And now we can transform the other data for the game via our encoder, passing the feature names as a parameter.
-    game_other_vectors = encoder.transform(game_other)
+    gameOtherVectors = encoder.transform(gameOther)
 
     # Again, we have to concatenate these into a single set of data.
-    game_vector = hstack([game_title_vector, game_other_vectors])
+    gameVector = hstack([gameTitleVector, gameOtherVectors])
 
     # And normalize the result so that it vibes with our other data.
-    game_vector_normalized = scaler.transform(game_vector)
+    gameVectorNormalized = scaler.transform(gameVector)
 
     # And finally we can predict our hypothetical game's sales based on the data
     # we've input. Voila!
     print("Creating sales prediction...")
-    predicted_sales = model.predict(game_vector_normalized)
+    predictedSales = model.predict(gameVectorNormalized)
     print("Voila!")
-    print(f"Predicted sales: ${', '.join(map(str,predicted_sales))} million.")
+    print(f"Predicted sales: ${', '.join(map(str,predictedSales))} million.")
 
-    # Calculate the r-squared value of the model
-    r2 = model.score(X_normalized, y)
+    # We're also going to calculate the r-squared of the model, just for kicks.
+    # I wanted to see how accurate the model was.
+    r2 = model.score(xNormalized, y)
     print(f"R-squared value of the model: {r2}.")
+
+
+if __name__ == "__main__":
+    main()
